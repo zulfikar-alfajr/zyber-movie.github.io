45600288091276ac410b17f472a6427f47271c4e
Ahmad Zulfikar Fajri <91066072+zulfikar-alfajr@users.noreply.github.com>
2022-06-27 19:57:58 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function searchMovie() {
   $("#movie-list").html("");
 
   $.ajax({
-    url: "http://omdbapi.com",
+    url: "https://omdbapi.com",
     type: "get",
     dataType: "json",
     data: {
@@ -65,7 +65,7 @@ $("#search-input").on("keyup", function (e) {
 
 $("#movie-list").on("click", ".see-detail", function () {
   $.ajax({
-    url: "http://omdbapi.com",
+    url: "https://omdbapi.com",
     type: "get",
     dataType: "json",
     data: {
